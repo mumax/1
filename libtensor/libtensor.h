d7ef4087f8e74cadbf249b230c1d65c924b6c3c9
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-02-17 16:40:57 +0000

    Experimenting with mixed C/C++ in libtensor, should remain callable from C.
    git-svn-id: file:///home/sim/svn/multiscale/trunk@9 e3891c05-e618-4c07-951a-b22c4065ae1c

--- libtensor/libtensor.h
+++ libtensor/libtensor.h
@@ -1,6 +1,7 @@
 #ifndef LIBTENSOR_H
 #define LIBTENSOR_H
 
+#include <stdlib.h>
 #include <stdio.h>
 
 // allow inclusion in C++ code
@@ -14,15 +15,12 @@ extern "C" {
 #ifndef DOUBLE_PRECISSION
 
 #define SINGLE_PRECISSION
-//#define DOUBLE_PRECISSION
 
 #endif
 #endif
 
 
-/**
- * We will probably do everything with single-precission floating point data. Nevertheless we use 'real' instead of float so that later we might still re-define it as double. 
- */
+/** We will probably do everything with single-precission floating point data. Nevertheless we use 'real' instead of float so that later we might still re-define it as double. */
 #ifdef SINGLE_PRECISSION
 typedef float real;
 #endif
@@ -32,6 +30,18 @@ typedef double real;
 #endif
 
 
+typedef struct{
+   int rank;			// tensor rank
+   int* size; 			// array of length rank, stores dimensions in all directions 
+   float* list;			// data as a continous array of length size[0]*size[1]*...
+}tensor;
+
+
+/** Creates a new tensor with given rank and size. Allocates the neccesary space for the elements. */
+tensor* new_tensor(int rank, int* size);
+
+/** Prints the tensor as ascii text */
+void print_tensor(tensor* t, FILE* out);
 
 #ifdef __cplusplus
 }
