6b478e8e8a2c7100e51bfb7f3de5621e3b2042cc
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-02-22 09:34:40 +0000

    Added tensor_component() to extract one component of a vector field, and represent it again as a tensor (of rank n-1)
    git-svn-id: file:///home/sim/svn/multiscale/trunk@23 e3891c05-e618-4c07-951a-b22c4065ae1c

--- libtensor/libtensor.cpp
+++ libtensor/libtensor.cpp
@@ -25,6 +25,21 @@ tensor* new_tensor(int rank, ...){
 }
 
 
+tensor* new_tensorN(int rank, int* size){
+  tensor* t = (tensor*)malloc(sizeof(tensor));
+  t->rank = rank;
+  t->size = (int*)calloc(rank, sizeof(int32_t));
+  
+  for(int i=0; i<rank; i++){
+    t->size[i] = size[i];
+  }
+ 
+  t-> list = (float*)calloc(tensor_length(t), sizeof(float));
+  
+  return t;
+}
+
+
 int tensor_index(tensor* t, int* indexarray){
   int index = indexarray[0];
   //AssertMsg(! (indexarray[0] < 0 || indexarray[0] >= size[0]), "Index out of range");
@@ -67,6 +82,21 @@ void write_tensor(tensor* t, FILE* out){
   // todo: error handling
 }
 
+void write_tensor_ascii(tensor* t, FILE* out){
+  fprintf(out, "%d\n", t->rank);
+  for(int i=0; i<t->rank; i++){
+    fprintf(out, "%d\n", t->size[i]);
+  }
+  for(int i=0; i<tensor_length(t); i++){
+    fprintf(out, "%f\n", t->list[i]);
+  }
+}
+
+void print_tensor(tensor* t){
+  write_tensor_ascii(t, stdout);
+}
+
+
 tensor* read_tensor(FILE* in){
   tensor* t = (tensor*)malloc(sizeof(tensor));
   fread(&(t->rank), sizeof(int32_t), 1, in);
@@ -75,4 +105,31 @@ tensor* read_tensor(FILE* in){
   t-> list = (float*)calloc(tensor_length(t), sizeof(float));
   fread(t->list, sizeof(float), tensor_length(t), in);
   return t;
+}
+
+tensor* tensor_component(tensor* t, int component){
+  int* size = new int[t->rank-1];
+  for(int i=0; i<t->rank-1; i++){
+    size[i] = t->size[i+1];
+  }
+  tensor* slice = new_tensorN(t->rank-1, size);
+  delete[] size;
+  int* index = new int[t->rank];
+  for(int i=1; i<t->rank; i++){
+    index[i] = 0;
+  }
+  index[0] = component;
+  slice->list = tensor_elem(t, index);
+  delete[] index;
+  return slice;
+}
+
+void delete_tensor_component(tensor* t){
+  // for safety, we invalidate the tensor so we'd quickly notice accidental use after freeing.
+  t->rank = -1;
+  t->size = NULL;
+  t->list = NULL;
+  free(t->size);
+  // we do not free t->list as it is owned by the parent tensor who may still be using it.
+  free(t);
 }
--- libtensor/libtensor.h
+++ libtensor/libtensor.h
@@ -25,13 +25,15 @@ typedef struct{
 /** Creates a new tensor with given rank and size (as integer vararg). Allocates the neccesary space for the elements. */
 tensor* new_tensor(int rank, ...);
 
-/** The same as new_tensor(), but with the size given as an array */
-//tensor* new_tensorN(int rank, int* size);
+/** The same as new_tensor(), but with the size given as an array. This is handy when the rank is not know at compile time. */
+tensor* new_tensorN(int rank, int* size);
 
-/** An easy way to acces tensor's elements is to view it as an N-dimensional array. */
-// void* tensor_array(tensor* t){
-// 
-// }
+/** Frees everything. */
+void delete_tensor(tensor* t);
+
+
+/** Returns the total number of elements in the tensor: size[0]*size[1]*...*size[rank-1]. This is also the length of the contiguous array that stores the tensor data. */
+int tensor_length(tensor* t);
 
 
 /** Returns the address of element i,j,k,... inside the tensor. This can be used to set or get elements form the tensor. */
@@ -40,17 +42,30 @@ float* tensor_elem(tensor* t, int* index);
 /** Given an N-dimensional index (i, j, k, ...), this function calculates the 1-dimensional index in the corresponding array that stores the tensor data. Thus, tensor_elem(i,j,k) is equivalent to list[tensor_index(i,j,k)]. */ 
 int tensor_index(tensor* t, int* indexarray);
 
-/** Returns the total number of elements in the tensor: size[0]*size[1]*...*size[rank-1]. This is also the length of the contiguous array that stores the tensor data. */
-int tensor_length(tensor* t);
+/** An easy way to acces tensor's elements is to view it as an N-dimensional array. */
+// void* tensor_array(tensor* t){
+// 
+// }
 
-/** Frees everything. */
-void delete_tensor(tensor* t);
 
 /** Writes the tensor in binary format */
 void write_tensor(tensor* t, FILE* out);
 
 /** Reads the tensor from binary format */
 tensor* read_tensor(FILE* in);
+
+/** Prints the tensor as ascii text. Can also be used to print to the screen: write_tensor_ascii(tensor, stdout); */
+void write_tensor_ascii(tensor* t, FILE* out);
+
+/** Prints the tensor to standard output. */
+void print_tensor(tensor* t);
+
+
+/** Makes a slice of a tensor, sharing the data with the original. */
+tensor* tensor_component(tensor* t, int component);
+
+/** Tensors created by tensor_component() should not be freed with delete_tensor() but with delete_tensor_component(), as the parent tensor may still be using the data list. */
+void delete_tensor_component(tensor* t);
 
 #ifdef __cplusplus
 }
