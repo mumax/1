356fd415862dcf07cba42776f6be3a40c938fdfe
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-02-25 10:46:28 +0000

    Building requires /usr/local/cuda/lib64 AND /usr/local/cuda/lib in your LD_LIBRARY_PATH, see all.bash
    git-svn-id: file:///home/sim/svn/multiscale/trunk@34 e3891c05-e618-4c07-951a-b22c4065ae1c

--- all.bash
+++ all.bash
@@ -1,5 +1,6 @@
-#export SIMROOT=$(pwd)
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SIMROOT/lib
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cuda/lib
+
 make clean
 if make; then
   make test;
