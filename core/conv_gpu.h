0546e40823f58337886b5ceba32ba2e1383ddd32
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-03-18 08:16:56 +0000

    core makefile compiles CUDA files
    git-svn-id: file:///home/sim/svn/multiscale/trunk@63 e3891c05-e618-4c07-951a-b22c4065ae1c

---
+++ core/conv_gpu.h
@@ -0,0 +1,20 @@
+#ifndef CONV_GPU_H
+#define CONV_GPU_H
+
+#include <stdio.h>
+
+// allow inclusion in C++ code
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ core/conv_test.cpp
@@ -0,0 +1,15 @@
+/**
+ * This program runs the Unittests for libtensor and illustrates its functions.
+ */
+#include "tensor.h"
+#include "conv_gpu.h"
+#include <assert.h>
+
+int main(int argc, char** argv){
+    printf("conv_test: ");
+    
+   
+    
+    printf("PASS\n");
+   return 0;
+}
