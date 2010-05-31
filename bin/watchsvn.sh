1a10eb2138001535d91f3b98d05532fad817ddd6
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-05-31 09:51:21 +0000

    Commented watchsvn.sh
    git-svn-id: file:///home/sim/svn/multiscale/trunk@161 e3891c05-e618-4c07-951a-b22c4065ae1c

--- bin/watchsvn.sh
+++ bin/watchsvn.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
-INTERVAL=60
-
-
+#
+# This utility periodically checks the subversion status of the current directory
+# and displays a pop-up notification when a new update is available.
+# 
+# Usage:
+#
+# First make sure libnotify is installed:
+# sudo apt-get install libnotify-bin
+# then run:
+# watchsvn.sh &
+#
+INTERVAL=120 #check every X seconds.
 
 
 (for (( ;; )); do
@@ -10,7 +19,7 @@ INTERVAL=60
   REMOTE=$(svn stat -uq | grep "Status against revision:" | tr -d "Status against revision:\t");
 
   if ((( $LOCAL != $REMOTE ))); then
-    echo "New SVN update available: revision" $REMOTE
+#    echo "New SVN update available: revision" $REMOTE
     notify-send "New SVN update available: " $REMOTE
     while ((( $LOCAL != $REMOTE ))); do
       sleep $INTERVAL
@@ -20,7 +29,7 @@ INTERVAL=60
 
     done;
   else
-    echo up to date, at revison: $LOCAL
+#    echo up to date, at revison: $LOCAL
     sleep $INTERVAL;
   fi;
 done;)
