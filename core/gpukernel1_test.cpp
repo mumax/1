85a01e927d81d4fb2df32583eafb6efd3f26e305
ben <ben@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-06-03 11:51:49 +0000

    svn problemsu in progress
    git-svn-id: file:///home/sim/svn/multiscale/trunk@185 e3891c05-e618-4c07-951a-b22c4065ae1c

--- core/gpukernel1_test.cpp
+++ core/gpukernel1_test.cpp
@@ -23,14 +23,17 @@ int main(int argc, char** argv){
   zero_pad[X] = 0;
   zero_pad[Y] = 0;
   zero_pad[Z] = 0;
-	
+
+  int* repetition = new int[3];
+	repetion[X] = repetition[Y] = repetition[Z] = 0;
+
 	float* FD_cell_size = new float[3];
 	FD_cell_size[X] = 1.0;
 	FD_cell_size[Y] = 1.0;
 	FD_cell_size[Z] = 1.0;
 
 	tensor *kernel;
-	init_Greens_kernel1(kernel, N0, N1, N2, zero_pad, FD_cell_size);
+	init_Greens_kernel1(kernel, N0, N1, N2, zero_pad, repetition, FD_cell_size);
 
 	fprintf(stderr, "PASS\n");
 
