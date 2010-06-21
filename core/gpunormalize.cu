caf49e82f5204312fd202e662a20f2322fd2835a
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-06-21 08:12:07 +0000

    implemented gpunormalize
    git-svn-id: file:///home/sim/svn/multiscale/trunk@296 e3891c05-e618-4c07-951a-b22c4065ae1c

--- core/gpunormalize.cu
+++ core/gpunormalize.cu
@@ -4,6 +4,30 @@
 extern "C" {
 #endif
 
+__global__ void _gpu_normalize(float* mx , float* my , float* mz){
+  int i = ((blockIdx.x * blockDim.x) + threadIdx.x);
+  float norm = rsqrtf(mx[i]*mx[i] + my[i]*my[i] + mz[i]*mz[i]);     // inverse square root
+  mx[i] *= norm;
+  my[i] *= norm;
+  mz[i] *= norm;
+}
+
+void gpu_normalize(tensor* m){
+
+  int complen = m->len / 3;
+    
+  int gridSize = -1, blockSize = -1;
+  make1dconf(complen, &gridSize, &blockSize);
+
+  float* mx = &(m->list[0*complen]);
+  float* my = &(m->list[1*complen]);
+  float* mz = &(m->list[2*complen]);
+  
+  timer_start("normalize");
+  _gpu_normalize<<<gridSize, blockSize>>>(mx, my, mz);
+  cudaThreadSynchronize();
+  timer_stop("normalize");
+}
 
 #ifdef __cplusplus
 }
--- core/gpunormalize.h
+++ core/gpunormalize.h
@@ -9,9 +9,20 @@
 #ifndef GPUNORMALIZE_H
 #define GPUNORMALIZE_H
 
+#include "tensor.h"
+#include "gputil.h"
+#include "timer.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+
+void gpu_normalize(tensor* m);
+
+
+/// @internal
+__global__ void _gpu_normalize(float* mx , float* my , float* mz);
 
 
 #ifdef __cplusplus
