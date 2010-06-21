adc740c05e248f88ea13d24cb1da4859deace65e
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-06-21 08:12:11 +0000

    implemented space-dependent normalization
    git-svn-id: file:///home/sim/svn/multiscale/trunk@298 e3891c05-e618-4c07-951a-b22c4065ae1c

--- core/gpunormalize.cu
+++ core/gpunormalize.cu
@@ -4,6 +4,30 @@
 extern "C" {
 #endif
 
+
+void gpu_normalize(param* p, tensor* m){
+
+  int complen = m->len / 3;
+
+  int gridSize = -1, blockSize = -1;
+  make1dconf(complen, &gridSize, &blockSize);
+
+  float* mx = &(m->list[0*complen]);
+  float* my = &(m->list[1*complen]);
+  float* mz = &(m->list[2*complen]);
+
+  timer_start("normalize");
+  if(p->msatMap == NULL){
+    _gpu_normalize<<<gridSize, blockSize>>>(mx, my, mz);
+  }
+  else{
+    _gpu_normalize_map<<<gridSize, blockSize>>>(mx, my, mz, p->msatMap->list);
+  }
+  cudaThreadSynchronize();
+  timer_stop("normalize");
+}
+
+
 __global__ void _gpu_normalize(float* mx , float* my , float* mz){
   int i = ((blockIdx.x * blockDim.x) + threadIdx.x);
   float norm = rsqrtf(mx[i]*mx[i] + my[i]*my[i] + mz[i]*mz[i]);     // inverse square root
@@ -12,22 +36,16 @@ __global__ void _gpu_normalize(float* mx , float* my , float* mz){
   mz[i] *= norm;
 }
 
-void gpu_normalize(tensor* m){
 
-  int complen = m->len / 3;
-    
-  int gridSize = -1, blockSize = -1;
-  make1dconf(complen, &gridSize, &blockSize);
-
-  float* mx = &(m->list[0*complen]);
-  float* my = &(m->list[1*complen]);
-  float* mz = &(m->list[2*complen]);
-  
-  timer_start("normalize");
-  _gpu_normalize<<<gridSize, blockSize>>>(mx, my, mz);
-  cudaThreadSynchronize();
-  timer_stop("normalize");
+__global__ void _gpu_normalize_map(float* mx , float* my , float* mz, float* normMap){
+  int i = ((blockIdx.x * blockDim.x) + threadIdx.x);
+  float norm = rsqrtf(mx[i]*mx[i] + my[i]*my[i] + mz[i]*mz[i]) * normMap[i];
+  mx[i] *= norm;
+  my[i] *= norm;
+  mz[i] *= norm;
 }
+
+
 
 #ifdef __cplusplus
 }
--- core/gpunormalize.h
+++ core/gpunormalize.h
@@ -10,6 +10,7 @@
 #define GPUNORMALIZE_H
 
 #include "tensor.h"
+#include "param.h"
 #include "gputil.h"
 #include "timer.h"
 
@@ -18,12 +19,14 @@ extern "C" {
 #endif
 
 
-void gpu_normalize(tensor* m);
+void gpu_normalize(param* p, tensor* m);
 
 
 /// @internal
 __global__ void _gpu_normalize(float* mx , float* my , float* mz);
 
+/// @internal
+__global__ void _gpu_normalize_map(float* mx , float* my , float* mz, float* normMap);
 
 #ifdef __cplusplus
 }
