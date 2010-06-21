4f8b2bfc07e7fb4cc7cf5bc6d2f7988c18825a81
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-06-21 10:12:11 +0000

    commented gpunormalize.h
    git-svn-id: file:///home/sim/svn/multiscale/trunk@300 e3891c05-e618-4c07-951a-b22c4065ae1c

--- core/gpunormalize.h
+++ core/gpunormalize.h
@@ -18,14 +18,18 @@
 extern "C" {
 #endif
 
+/**
+ * Normalizes the magnetization (or any other tensor).
+ */
+void gpu_normalize(param* p,    ///< parameters indicate whether normalization is space-dependent and store msatMap if applicable.
+                   tensor* m    ///< 3 x N0 x N1 x N2 tensor to normalize
+                   );
 
-void gpu_normalize(param* p, tensor* m);
 
-
-/// @internal
+/// @internal uniform normalization
 __global__ void _gpu_normalize(float* mx , float* my , float* mz);
 
-/// @internal
+/// @internal space-dependent normalization
 __global__ void _gpu_normalize_map(float* mx , float* my , float* mz, float* normMap);
 
 #ifdef __cplusplus
