7c7b4db873d78ca83b0a7675d8e91a49d4c4fb55
arne <arne@e3891c05-e618-4c07-951a-b22c4065ae1c>
2010-06-21 10:12:17 +0000

    commented field.h
    git-svn-id: file:///home/sim/svn/multiscale/trunk@303 e3891c05-e618-4c07-951a-b22c4065ae1c

--- core/timestep.cpp
+++ core/timestep.cpp
@@ -12,6 +12,7 @@ timestepper *new_timestepper(param *params, fieldplan* field){
   ts->h = new_gputensor(4, tensor_size4D(params->size));
   
   int solverType = ts->params->solverType;
+  
   if(solverType == SOLVER_EULER){
     ts->solver = new_gpueuler(params);
   }
@@ -30,13 +31,18 @@ timestepper *new_timestepper(param *params, fieldplan* field){
 void timestep(timestepper *ts, tensor *m, double *totalTime){
 
   int solverType = ts->params->solverType;
+  
   if(solverType == SOLVER_EULER){
-    gpueuler* euler = (gpueuler*)ts->solver;
+    
+    //gpueuler* euler = (gpueuler*)ts->solver;
     //gpueuler_step(euler, m, h, totalTime);
+    
   }
   else if(solverType == SOLVER_HEUN){
+    
     gpuheun* heun = (gpuheun*)ts->solver;
     gpuheun_step(heun, m, ts->h, totalTime);
+    
   }
   else{
     fprintf(stderr, "Unknown solver type: %d\n", ts->params->solverType);
