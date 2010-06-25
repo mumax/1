6bf2fa067b72ca51e73474ba3d220b6695043534
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-06-25 17:37:32 +0200

    Arne's weekend begins

--- app/go-frontend/gpu/conv.go
+++ app/go-frontend/gpu/conv.go
@@ -6,14 +6,21 @@ import(
 
 
 type Conv struct{
-  fft *FFT;
-  kernel [3][3]*Tensor
+//   dataSize   [3]int                 ///< size of the magnetization and field, this becomes the FFT dataSize
+//   logicSize  [3]int                 ///< size of magnetization + padding zeros, this becomes the FFT logicSize
+//   physicSize [3]int                 ///< logic size + even more padding zeros 
+  kernel     [3][3]*Tensor             ///< should have paddedSize
+  fft       *FFT;
 }
 
 
-func NewConv(kernel [3][3]*Tensor) *Conv{
+func NewConv(dataSize []int, kernel [3][3]*Tensor) *Conv{
+  ///@todo size check
+  
   conv := new(Conv)
 
+  for i:=range dataSize { conv.dataSize[i] = dataSize[i] }
+  
   return conv
 }
 
