3284e33a6af1e58f2c80902b3fd8323441fe49b5
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-06-28 10:40:04 +0200

    conv.go calculates dipole field nicely!

--- app/go-frontend/gpu/conv_test.go
+++ app/go-frontend/gpu/conv_test.go
@@ -2,7 +2,7 @@ package gpu
 
 import(
    "testing"
- //   "sim"
+    "sim"
     "tensor"
     "fmt"
     "os"
@@ -10,17 +10,19 @@ import(
 
 func TestConv(t *testing.T){
   OverrideStride(1)
-  size4D := []int{3, 4, 4, 2}
+  size4D := []int{3, 32, 32, 2}
   size := size4D[1:]
   kernelSize := []int{2*size[X], 2*size[Y], 2*size[Z]}
   
   conv := NewConv(size, kernelSize)
-  //kernel := sim.FaceKernel6(size, []float{1., 1., 1.})
-  kernel := make([]*tensor.Tensor3, 6)
-  for i := range kernel{
-    kernel[i] = tensor.NewTensor3(kernelSize)
-  }
-  kernel[XX].List()[0] = 1.
+  kernel := sim.FaceKernel6(size, []float{1., 1., 1.})
+
+  // unit kernel
+//   kernel := make([]*tensor.Tensor3, 6)
+//   for i := range kernel{
+//     kernel[i] = tensor.NewTensor3(kernelSize)
+//   }
+//   kernel[XX].List()[0] = 1.
   
 //   for i,k:= range kernel{
 //     fmt.Println("kernel", i, k.Size())
@@ -41,7 +43,7 @@ func TestConv(t *testing.T){
 
   m, h := NewTensor(size4D), NewTensor(size4D)
 
-  m.Set([]int{0, 0, 0, 0}, 1.)
+  m.Set([]int{0, 7, 7, 0}, 1.)
   tensor.WriteFile("m.t", m)
   conv.Exec(m, h)
   tensor.WriteFile("h.t", h)
