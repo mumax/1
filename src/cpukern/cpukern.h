ef550e8a6b73fd560a1fc91bbb86acb8a308f5d0
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-08-04 15:08:42 +0200

    cpukern.h

---
+++ src/cpukern/cpukern.h
@@ -0,0 +1,23 @@
+/**
+ * @file
+ *
+ * This file contains all the (public) includes for libcpukern.
+ *
+ * @todo put central documentation here
+ *
+ * @author Arne Vansteenkiste
+ * @author Ben Van de Wiele
+ */
+
+
+#include "cpu_fft.h"
+#include "cpu_kernmul.h"
+#include "cpu_linalg.h"
+#include "cpu_mem.h"
+#include "cpu_normalize.h"
+#include "cpu_tensor.h"
+#include "cpu_torque.h"
+#include "cpu_transpose.h"
+#include "cpu_zeropad.h"
+#include "cpu_properties.h"
+#include "cpu_anal.h"
