122a4c46f16a1ab172f2ab92471d0c1b8015717f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-08-04 22:14:01 +0200

    cpu_anal

---
+++ src/cpukern/cpu_anal.h
@@ -0,0 +1,19 @@
+/**
+ * @file
+ *
+ * @author Ben Van de Wiele
+ * @author Arne Vansteenkiste
+ */
+#ifndef gpu_anal_h
+#define gpu_anal_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void cpu_anal_fw_step_unsafe(float* m, float* h, float dt, float alpha, int N);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
