6b93a277e12bd47736b96666bc838dd08d4612d8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-08-19 11:28:04 +0200

    autosave can handle binary/ascii/... formats

--- src/go/sim/simoutput.go
+++ src/go/sim/simoutput.go
@@ -20,37 +20,65 @@ func (s *Sim) OutputDir(outputdir string) {
 }
 
 
-
-type Output interface{
-  NeedSave(time float) bool
-  Save(sim *Sim)
+type Output interface {
+	NeedSave(time float) bool
+	Save(sim *Sim)
 }
 
 
-type Periodic struct{
-  period float
-  sinceoutput float
+type Periodic struct {
+	period      float
+	sinceoutput float
 }
 
-func (p *Periodic) NeedSave(time float) bool{
-  return time - p.sinceoutput >= p.period
+func (p *Periodic) NeedSave(time float) bool {
+	return time-p.sinceoutput >= p.period
 }
 
-type MOutput struct{
-  *Periodic
+type MBinary struct {
+	*Periodic
 }
 
-func (m *MOutput) Save(s *Sim){
-  fname := s.outputdir + "/" + "m" + fmt.Sprintf("%06d", s.autosaveIdx) + ".t"
-  tensor.WriteFile(fname, s.m)
-  m.sinceoutput = s.time
+func (m *MBinary) Save(s *Sim) {
+	fname := s.outputdir + "/" + "m" + fmt.Sprintf("%06d", s.autosaveIdx) + ".t"
+	tensor.WriteFile(fname, s.m)
+	m.sinceoutput = s.time
+}
+
+type MAscii struct {
+	*Periodic
+}
+
+func (m *MAscii) Save(s *Sim) {
+	fname := s.outputdir + "/" + "m" + fmt.Sprintf("%06d", s.autosaveIdx) + ".txt"
+	out, err := os.Open(fname, os.O_WRONLY | os.O_CREAT, 0666)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-2)
+	}
+	defer out.Close()
+	tensor.Format(out, s.m)
+
+	m.sinceoutput = s.time
 }
 
 
-func (s *Sim) Autosave(what string, interval float){
-  //TODO other than m
-  s.outschedule = s.outschedule[0:len(s.outschedule)+1]
-  s.outschedule[len(s.outschedule)-1] = &MOutput{&Periodic{interval, 0.}}
+func (s *Sim) Autosave(what, format string, interval float) {
+	s.outschedule = s.outschedule[0 : len(s.outschedule)+1]
+	switch what {
+	case "m":
+		switch format {
+		case "binary":
+			s.outschedule[len(s.outschedule)-1] = &MBinary{&Periodic{interval, 0.}}
+		case "ascii":
+			s.outschedule[len(s.outschedule)-1] = &MAscii{&Periodic{interval, 0.}}
+		case "png":
+		default:
+			panic("unknown format " + format + ". options are: binary, ascii, png")
+		}
+	default:
+		panic("unknown output quantity " + what + ". options are: m")
+	}
 }
 
 // func (s *Sim) autosavem() {
