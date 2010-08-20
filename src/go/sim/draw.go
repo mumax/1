9ff54f71cdb33405cbcfc1225d910deee45fc1fa
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-08-20 09:54:06 +0200

    color png support

--- src/go/sim/draw.go
+++ src/go/sim/draw.go
@@ -4,6 +4,7 @@ import (
 	"tensor"
 	. "image"
 	"image/png"
+	"math"
 	// 	"os"
 	// 	"fmt"
 	"io"
@@ -26,22 +27,41 @@ func PNG(out io.Writer, t tensor.Tensor) {
 }
 
 func DrawTensor(t tensor.Tensor) *NRGBA {
-    // todo: we need to handle any rank <= 4
-    for tensor.Rank(t) > 2{
-      t = tensor.Slice(t, 0, 0)
-    } 
-  
-	w, h := t.Size()[0], t.Size()[1]
-	img := NewNRGBA(w, h)
-	pos := []int{w, h}
-	for i := 0; i < w; i++ {
-		pos[X] = i
-		for j := 0; j < h; j++ {
-			pos[Y] = j
-			img.Set(i, j, GreyMap(-1., 1., t.Get(pos)))
-		}
+	// todo: we need to handle any rank
+	for tensor.Rank(t) == 4 {
+		t = tensor.Average(t, 1) //average over the thickness
 	}
-	return img
+
+// 	w, h := t.Size()[0], t.Size()[1]
+// 	img := NewNRGBA(w, h)
+// 	pos := []int{w, h}
+// 	for i := 0; i < w; i++ {
+// 		pos[X] = i
+// 		for j := 0; j < h; j++ {
+// 			pos[Y] = j
+// 			img.Set(i, j, GreyMap(-1., 1., t.Get(pos)))
+// 		}
+// 	}
+// 	return img
+
+ w, h := t.Size()[1], t.Size()[2]
+ img := NewNRGBA(w, h)
+ pos := []int{0, w, h}
+ for i := 0; i < w; i++ {
+   pos[1] = i
+   for j := 0; j < h; j++ {
+     pos[2] = j
+     pos[0] = X
+     x := t.Get(pos)
+     pos[0] = Y
+     y := t.Get(pos)
+     pos[0] = Z
+     z := t.Get(pos)
+     img.Set(i, j, HSLMap(z, y, x)) // TODO: x is thickness for now...
+   }
+ }
+ return img
+
 }
 
 func GreyMap(min, max, value float) NRGBAColor {
@@ -54,4 +74,86 @@ func GreyMap(min, max, value float) NRGBAColor {
 	}
 	color8 := uint8(255 * color)
 	return NRGBAColor{color8, color8, color8, 255}
+}
+
+func HSLMap(x, y, z float) NRGBAColor{
+  s := fsqrt(x*x + y*y + z*z)
+  l := 0.5 * z + 0.5
+  h := float(math.Atan2(float64(y), float64(x)))
+  return HSL(h, s, l)
+}
+
+func fsqrt(number float) float{
+  return float(math.Sqrt(float64(number)))
+}
+
+// h = 0..2pi, s=0..1, l=0..1
+func HSL(h, s, l float) NRGBAColor {
+	if s > 1 {
+		s = 1
+	}
+	if l > 1 {
+		l = 1
+	}
+	for h < 0 {
+		h += 2 * math.Pi
+	}
+	for h > 2*math.Pi {
+		h -= math.Pi
+	}
+	h = h * (180.0 / math.Pi / 60.0)
+
+	// chroma
+	var c float
+	if l <= 0.5 {
+		c = 2 * l * s
+	} else {
+		c = (2 - 2*l) * s
+	}
+
+	x := c * (1 - abs(fmod(h, 2)-1))
+
+	var (
+		r, g, b float
+	)
+
+	switch {
+	case 0 <= h && h < 1:
+		r, g, b = c, x, 0.
+	case 1 <= h && h < 2:
+		r, g, b = x, c, 0.
+	case 2 <= h && h < 3:
+		r, g, b = 0., c, x
+	case 3 <= h && h < 4:
+		r, g, b = 0, x, c
+	case 4 <= h && h < 5:
+		r, g, b = x, 0., c
+	case 5 <= h && h < 6:
+		r, g, b = c, 0., x
+	default:
+		r, g, b = 0., 0., 0.
+	}
+
+	m := l - 0.5*c
+	r, g, b = r+m, g+m, b+m
+	R, G, B := uint8(255*r), uint8(255*g), uint8(255*b)
+	return NRGBAColor{R, G, B, 255}
+}
+
+// modulo
+func fmod(number, mod float) float {
+	for number < mod {
+		number += mod
+	}
+	for number > mod {
+		number -= mod
+	}
+	return number
+}
+
+func abs(number float) float {
+	if number < 0 {
+		return -number
+	}// else
+		return number
 }
