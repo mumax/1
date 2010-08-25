bdb8472722e00aa184902ef4d3cbf8b8ac248a1c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-08-25 15:30:02 +0200

    cleaning up

--- src/go/main/main.go
+++ src/go/main/main.go
@@ -26,25 +26,33 @@ func main() {
   flag.Parse()
   
 	if *server{
-    SimServerMain()
-    os.Exit(0)
+    main_slave()
+  }else{
+    main_master()
+  }
+}
+
+// when running in the normal "master" mode, i.e. given an input file to process locally
+func main_master(){
+  if flag.NArg() == 0 {
+    fmt.Fprintln(os.Stderr, "No input files.")
+    os.Exit(-1)
   }
 
+  for i := 0; i < flag.NArg(); i++ {
+    in, err := os.Open(flag.Arg(i), os.O_RDONLY, 0666)
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err)
+      os.Exit(-2)
+    }
+    defer in.Close()
+    exec(in)
+  }
+}
 
-	if flag.NArg() == 0 {
-		fmt.Fprintln(os.Stderr, "No input files.")
-		os.Exit(-1)
-	}
-
-	for i := 0; i < flag.NArg(); i++ {
-		in, err := os.Open(flag.Arg(i), os.O_RDONLY, 0666)
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(-2)
-		}
-		defer in.Close()
-		exec(in)
-	}
+// when running in "slave" mode, i.e. accepting commands over the network as part of a cluster
+func main_slave(){
+  SimServerMain() // is in package sim for visibility reasons
 }
 
 
