2e61d565756479aea22efe0807ed201da2e50c35
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-09-07 15:41:44 +0200

    gpu_stream stub

---
+++ src/gpukern/gpu_stream.cpp
@@ -0,0 +1,11 @@
+#include "gpu_stream.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/gpukern/gpu_stream.h
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * Central place where GPU streams are managed
+ * 
+ * @author Arne Vansteenkiste
+ */
+#ifndef gpu_stream_h
+#define gpu_stream_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
