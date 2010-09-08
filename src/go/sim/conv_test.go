42edc4479daf26f8d9c0d2f54afd8c8fb9b2660c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-09-08 12:22:43 +0200

    conv_test to compare with branch arne

--- src/go/sim/conv_test.go
+++ src/go/sim/conv_test.go
@@ -9,27 +9,19 @@ import (
 
 func TestConv(t *testing.T) {
 
-	size4D := []int{3, 1, 32, 32}
+	size4D := []int{3, 1, 8, 8}
 	size := size4D[1:]
 	kernelSize := padSize(size)
 
 	kernel := FaceKernel6(kernelSize, []float{1., 1., 1.}, 8)
 	conv := NewConv(backend, size, kernel)
 
-	//   for i,k:= range kernel{
-	//     fmt.Println("kernel", i, k.Size())
-	//   }
-	//   fmt.Println("conv", conv.KernelSize())
-	//   fmt.Println("conv.fft", conv.fft)
-
-	//conv.LoadKernel6(kernel)
-
-	  for i:=range(conv.kernel){
-      fmt.Println("K", i)
-	    if conv.kernel[i] != nil{
-	      tensor.Format(os.Stdout, conv.kernel[i])
-	    }
-	  }
+	for i := range conv.kernel {
+		fmt.Println("K", i)
+		if conv.kernel[i] != nil {
+			tensor.Format(os.Stdout, conv.kernel[i])
+		}
+	}
 
 	m, h := NewTensor(backend, size4D), NewTensor(backend, size4D)
 
