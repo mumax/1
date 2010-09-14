bb9d7c463cb669a182c6dca4f9f5ddc127c7cbaf
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-09-14 14:29:51 +0200

    pulled Ben's gpu_fft to gpukern/gpu_fft3

---
+++ src/gpukern/gpu_fft3.cu
@@ -0,0 +1,277 @@
+// #include "gpufft2.h"
+#include "gpu_fft.h"
+#include "gputil.h"
+#include "timer.h"
+#include <stdio.h>
+#include <assert.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+gpuFFT3dPlan* new_gpuFFT3dPlan_padded(int* size, int* paddedSize){
+  
+  int N0 = size[X];
+  int N1 = size[Y];
+  int N2 = size[Z];
+  
+  assert(paddedSize[X] > 0);
+  assert(paddedSize[Y] > 1);
+  assert(paddedSize[Z] > 1);
+  
+  gpuFFT3dPlan* plan = (gpuFFT3dPlan*)malloc(sizeof(gpuFFT3dPlan));
+  
+  plan->size = (int*)calloc(3, sizeof(int));    ///@todo not int* but int[3]
+  plan->paddedSize = (int*)calloc(3, sizeof(int));
+  plan->paddedStorageSize = (int*)calloc(3, sizeof(int));
+  
+//   int* paddedSize = plan->paddedSize;
+  int* paddedStorageSize = plan->paddedStorageSize;
+  
+  plan->size[0] = N0; 
+  plan->size[1] = N1; 
+  plan->size[2] = N2;
+  plan->N = N0 * N1 * N2;
+  
+  plan->paddedSize[X] = paddedSize[X];
+  plan->paddedSize[Y] = paddedSize[Y];
+  plan->paddedSize[Z] = paddedSize[Z];
+  plan->paddedN = plan->paddedSize[0] * plan->paddedSize[1] * plan->paddedSize[2];
+  
+  plan->paddedStorageSize[X] = plan->paddedSize[X];
+  plan->paddedStorageSize[Y] = plan->paddedSize[Y];
+  plan->paddedStorageSize[Z] = gpu_pad_to_stride( plan->paddedSize[Z] + 2 );
+  plan->paddedStorageN = paddedStorageSize[X] * paddedStorageSize[Y] * paddedStorageSize[Z];
+  
+  gpu_safe( cufftPlan1d(&(plan->fwPlanZ), plan->paddedSize[Z], CUFFT_R2C, size[X]*size[Y]) );
+  gpu_safe( cufftPlan1d(&(plan->planY), plan->paddedSize[Y], CUFFT_C2C, paddedStorageSize[Z] * size[X] / 2) );          // IMPORTANT: the /2 is necessary because the complex transforms have only half the amount of elements (the elements are now complex numbers)
+  gpu_safe( cufftPlan1d(&(plan->planX), plan->paddedSize[X], CUFFT_C2C, paddedStorageSize[Z] * paddedSize[Y] / 2) );
+  gpu_safe( cufftPlan1d(&(plan->invPlanZ), plan->paddedSize[Z], CUFFT_C2R, size[X]*size[Y]) );
+  
+  plan->transp = new_gpu_array(plan->paddedStorageN);
+  
+  return plan;
+}
+
+
+
+
+
+
+void gpuFFT3dPlan_forward(gpuFFT3dPlan* plan, tensor* input, tensor* output){
+  assertDevice(input->list);
+  assertDevice(output->list);
+  assert(input->list == output->list); ///@todo works only in-place for now
+  assert(input->rank == 3);
+  assert(output->rank == 3);
+  for(int i=0; i<3; i++){
+    assert( input->size[i] == plan->paddedStorageSize[i]);
+    assert(output->size[i] == plan->paddedStorageSize[i]);
+  }
+  
+  gpuFFT3dPlan_forward_unsafe(plan, input->list, output->list);
+
+  return;
+}
+
+void gpuFFT3dPlan_forward_unsafe(gpuFFT3dPlan* plan, float* input, float* output){
+  timer_start("gpu_plan3d_real_input_forward_exec");
+  
+  int* size = plan->size;
+  int* pSSize = plan->paddedStorageSize;
+  int N0 = pSSize[X];
+  int N1 = pSSize[Y];
+  int N2 = pSSize[Z]/2; // we treat the complex data as an N0 x N1 x N2 x 2 array
+  int N3 = 2;
+  
+  int half_pSSize = plan->paddedStorageN/2;
+  
+  float* data = input;
+  float* data2 = plan->transp; 
+
+  if ( pSSize[X]!=size[X] || pSSize[Y]!=size[Y]){
+      // out of place FFTs in Z-direction from the 0-element towards second half of the zeropadded matrix (out of place: no +2 on input!)
+    gpu_safe( cufftExecR2C(plan->fwPlanZ, (cufftReal*)data,  (cufftComplex*) (data + half_pSSize) ) );     // it's in data
+    cudaThreadSynchronize();
+      // zero out the input data points at the start of the matrix
+    gpu_zero(data, size[X]*size[Y]*pSSize[Z]);
+    
+      // YZ-transpose within the same matrix from the second half of the matrix towards the 0-element
+    yz_transpose_in_place_fw(data, size, pSSize);                                                          // it's in data
+    
+      // in place FFTs in Y-direction
+    gpu_safe( cufftExecC2C(plan->planY, (cufftComplex*)data,  (cufftComplex*)data, CUFFT_FORWARD) );       // it's in data 
+    cudaThreadSynchronize();
+  }
+  
+  else {          //no zero padding in X- and Y direction (e.g. for Greens kernel computations)
+      // in place FFTs in Z-direction (there is no zero space to perform them out of place)
+    gpu_safe( cufftExecR2C(plan->fwPlanZ, (cufftReal*)data,  (cufftComplex*) data ) );                     // it's in data
+    cudaThreadSynchronize();
+    
+      // YZ-transpose needs to be out of place.
+    gpu_transposeYZ_complex(data, data2, N0, N1, N2*N3);                                                   // it's in data2
+    
+      // perform the FFTs in the Y-direction
+    gpu_safe( cufftExecC2C(plan->planY, (cufftComplex*)data2,  (cufftComplex*)data, CUFFT_FORWARD) );      // it's in data
+    cudaThreadSynchronize();
+  }
+
+  if(N0 > 1){    // not done for 2D transforms
+      // XZ transpose still needs to be out of place
+    gpu_transposeXZ_complex(data, data2, N0, N2, N1*N3);                                                   // it's in data2
+    
+      // in place FFTs in X-direction
+    gpu_safe( cufftExecC2C(plan->planX, (cufftComplex*)data2,  (cufftComplex*)output, CUFFT_FORWARD) );    // it's in data
+    cudaThreadSynchronize();
+  }
+
+  timer_stop("gpu_plan3d_real_input_forward_exec");
+  
+  return;
+}
+
+
+
+
+
+
+
+void gpuFFT3dPlan_inverse(gpuFFT3dPlan* plan, tensor* input, tensor* output){
+  assertDevice(input->list);
+  assertDevice(output->list);
+  assert(input->list == output->list); ///@todo works only in-place for now
+  assert(input->rank == 3);
+  assert(output->rank == 3);
+  for(int i=0; i<3; i++){
+    assert( input->size[i] == plan->paddedStorageSize[i]);
+    assert(output->size[i] == plan->paddedStorageSize[i]);
+  }
+  gpuFFT3dPlan_inverse_unsafe(plan, input->list, output->list);
+  
+  return;
+}
+
+
+void gpuFFT3dPlan_inverse_unsafe(gpuFFT3dPlan* plan, float* input, float* output){
+  
+  timer_start("gpu_plan3d_real_input_inverse_exec");
+  
+  int* size = plan->size;
+  int* pSSize = plan->paddedStorageSize;
+  int N0 = pSSize[X];
+  int N1 = pSSize[Y];
+  int N2 = pSSize[Z]/2; // we treat the complex data as an N0 x N1 x N2 x 2 array
+  int N3 = 2;
+  
+  float* data = input;
+  float* data2 = plan->transp; // both the transpose and FFT are out-of-place between data and data2
+
+  if (N0 > 1){
+      // out of place FFTs in the X-direction (i.e. no +2 stride on input!)
+    gpu_safe( cufftExecC2C(plan->planX, (cufftComplex*)data,  (cufftComplex*)data2, CUFFT_INVERSE) );      // it's in data2
+    cudaThreadSynchronize();
+
+      // XZ transpose still needs to be out of place
+    gpu_transposeXZ_complex(data2, data, N1, N2, N0*N3);                                                   // it's in data
+  }
+  
+  if ( pSSize[X]!=size[X] || pSSize[Y]!=size[Y]){
+      // in place FFTs in Y-direction
+    gpu_safe( cufftExecC2C(plan->planY, (cufftComplex*)data,  (cufftComplex*)data, CUFFT_INVERSE) );        // it's in data
+    cudaThreadSynchronize();
+
+      // YZ-transpose within the same matrix from the 0-element towards the second half of the matrix
+    yz_transpose_in_place_inv(data, size, pSSize);                                                          // it's in data
+
+      // out of place FFTs in Z-direction from the second half of the matrix towards the 0-element
+    gpu_safe( cufftExecC2R(plan->invPlanZ, (cufftComplex*)(data + N0*N1*N2), (cufftReal*)data ));           // it's in data
+    cudaThreadSynchronize();
+
+  }
+  else {          //no zero padding in X- and Y direction (e.g. for Greens kernel computations)
+      // out of place FFTs in Y-direction
+    gpu_safe( cufftExecC2C(plan->planY, (cufftComplex*)data,  (cufftComplex*)data2, CUFFT_INVERSE) );       // it's in data
+    cudaThreadSynchronize();
+    
+      // YZ-transpose needs to be out of place.
+    gpu_transposeYZ_complex(data2, data, N0, N2, N1*N3);                                                    // it's in data2   
+
+      // in place FFTs in Z-direction
+    gpu_safe( cufftExecC2R(plan->invPlanZ, (cufftComplex*) data, (cufftReal*) data ));                      // it's in data
+    cudaThreadSynchronize();
+  }
+ 
+  timer_stop("gpu_plan3d_real_input_inverse_exec");
+  
+  return;
+}
+
+
+
+
+
+
+
+
+void yz_transpose_in_place_fw(float *data, int *size, int *pSSize){
+
+  int offset = pSSize[X]*pSSize[Y]*pSSize[Z]/2;    //start of second half
+  int pSSize_YZ = pSSize[Y]*pSSize[Z];
+
+  if (size[X]!=pSSize[X]){
+    for (int i=0; i<size[X]; i++){       // transpose each plane out of place: can be parallellized
+      int ind1 = offset + i*size[Y]*pSSize[Z];
+      int ind2 = i*pSSize_YZ;
+      gpu_transpose_complex(data + ind1, data + ind2, size[Y], pSSize[Z], 0, pSSize[Y]-size[Y]);
+    }
+    gpu_zero(data + offset, offset);     // possible to delete values in gpu_transpose_complex
+  }
+  else{     //padding in the y-direction
+    for (int i=0; i<size[X]-1; i++){       // transpose all but the last plane out of place: can only partly be parallellized
+      int ind1 = offset + i*size[Y]*pSSize[Z];
+      int ind2 = i*pSSize_YZ;
+      gpu_transpose_complex(data + ind1, data + ind2, size[Y], pSSize[Z], 0, pSSize[Y]-size[Y]);
+      gpu_zero(data + offset + i*size[Y]*pSSize[Z], size[Y]*pSSize[Z]);     // deletable
+    }
+    gpu_transpose_complex_in_plane_fw(data + (size[X]-1)*pSSize_YZ, size[Y], pSSize[Z]);
+  }
+  
+
+  return;
+}
+
+
+void yz_transpose_in_place_inv(float *data, int *size, int *pSSize){
+
+  int offset = pSSize[X]*pSSize[Y]*pSSize[Z]/2;    //start of second half
+  int pSSize_YZ = pSSize[Y]*pSSize[Z];
+
+  if (size[X]!=pSSize[X])
+      // transpose each plane out of place: can be parallellized
+    for (int i=0; i<size[X]; i++){
+      int ind1 = i*pSSize_YZ;
+      int ind2 = offset + i*size[Y]*pSSize[Z];
+      gpu_transpose_complex(data + ind1, data + ind2, pSSize[Z]/2, 2*size[Y], pSSize[Y]-size[Y], 0);
+    }
+  else{
+      // last plane needs to transposed in plane
+    gpu_transpose_complex_in_plane_inv(data + (size[X]-1)*pSSize_YZ, pSSize[Z]/2, 2*size[Y]);
+      // transpose all but the last plane out of place: can only partly be parallellized
+    for (int i=0; i<size[X]-1; i++){
+      int ind1 = i*pSSize_YZ;
+      int ind2 = offset + i*size[Y]*pSSize[Z];
+      gpu_transpose_complex(data + ind1, data + ind2, pSSize[Z]/2, 2*size[Y], pSSize[Y]-size[Y], 0);
+
+    }
+  }
+  
+  return;
+}
+
+
+#ifdef __cplusplus
+}
+#endif
---
+++ src/gpukern/gpu_fft3.h
@@ -0,0 +1,124 @@
+/**
+ * @file
+ *
+ * This file collects all FFT routines for possibly zeropadded matrices with 2D or 3D dimensions.
+ * The following is taken into account:
+ *    - real-to-complex FFTs
+ *    - No FFTs on rows which contain only zeros
+ *    - The CUCA memory access is aligned
+ *    - 2D FFTs on data padded in X- and/or Y-direction is performed in-place
+ *    
+ * @todo restrict the required extra memory for 3D to the minimum
+ * @todo concurrent execution?
+ 
+ * @author Ben Van de Wiele
+ * @author Arne Vansteenkiste
+ */
+#ifndef GPU_FFT_H
+#define GPU_FFT_H
+
+#include "tensor.h"
+#include "gputil.h"
+#include <cufft.h>
+#include <gpu_transpose.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/**
+* A real-to-complex FFT plan on the GPU.
+*/
+typedef struct{
+  int* size;               ///< logical size of the (real) input data
+  int N;                   ///< total number of floats in size
+  
+  int* paddedSize;         ///< size after zero-padding. @note zero-padding is conditional and not necessarily performed in each direction. Therefore, "paddedSize" is not necessarily twice "size".
+  int paddedN;             ///< total number of floats in paddedSize
+  
+  int* paddedStorageSize;  ///< A real-to-complex FFT requires padding with one complex number in the last dimension. However, is this would result in misalgned memory, we pad with (typically) 64 floats
+  int paddedStorageN;      ///< total number of floats in paddedStorageSize
+  
+  cufftHandle fwPlanZ;     ///< 1D real-to-complex plan for Z-direction
+  cufftHandle invPlanZ;    ///< 1D complex-to-real plan for Z-direction
+  cufftHandle planY;       ///< 1D complex-to-complex plan for Y-direction, forward or inverse
+  cufftHandle planX;       ///< 1D complex-to-complex plan for X-direction, forward or inverse
+  
+  float* transp;           ///< buffer for out-of-place transposing @todo: CAN WE REMOVE THIS ONE?
+  
+}gpuFFT3dPlan;
+
+
+
+/**
+ * Creates a new FFT plan for transforming real 2D or 3D data. 
+ * Zero-padding in each dimension is optional, and rows with only zero's are not transformed.
+ */
+gpuFFT3dPlan* new_gpuFFT3dPlan_padded(int* size, 
+                                      int* paddedSize
+                                      );
+
+/**
+ * Forward FFT of real possibly zero-padded data with 2D or 3D dimensions. FFTs on rows containing only zeros are not performed.
+ * The input is checked for compatibility: input data are tensors.
+ */
+void gpuFFT3dPlan_forward(gpuFFT3dPlan* plan, 
+                          tensor* input, 
+                          tensor* output
+                          );
+                          
+                          
+/**
+ * Forward FFT of real possibly zero-padded data with 2D or 3D dimensions. FFTs on rows containing only zeros are not performed.
+ * Routine is called 'unsafe' since the input is not checked for compatibility: input data are float arrays.
+ */
+void gpuFFT3dPlan_forward_unsafe(gpuFFT3dPlan* plan, 
+                                 float* input, 
+                                 float* output
+                                 );                          
+
+                                 
+                                 
+/**
+ * Forward FFT of real possibly zero-padded data with 2D or 3D dimensions. FFTs on rows containing only zeros are not performed.
+ * The input is checked for compatibility: input data are tensors.
+ */
+void gpuFFT3dPlan_inverse(gpuFFT3dPlan* plan, 
+                          tensor* input, 
+                          tensor* output);                            
+                                 
+                                 
+/**
+ * Inverse FFT of real possibly zero-padded data with 2D or 3D dimensions. FFTs on rows containing only zeros are not performed.
+ * Routine is called 'unsafe' since the input is not checked for compatibility: input data are float arrays.
+ */
+void gpuFFT3dPlan_inverse_unsafe(gpuFFT3dPlan* plan, 
+                                 float* input, 
+                                 float* output
+                                 );                               
+                                 
+
+/**
+ * In this routine, the input data 'data' is Fourier transformed in the Z-direction and stored contiguously starting from the 
+ * seconds half of the data array and will be transposed towards the first part of the matrix.  Then it is ready for the transform 
+ * in the Y-direction.  Zero-padding in X- and/or Y-direction is assumed.
+ */
+void yz_transpose_in_place_fw(float *data, 
+                              int *size, 
+                              int *pSSize
+                              );
+
+/**
+ * In this routine, all input FFT transformed data is stored non-contiguously starting from the first half of the  data array and 
+ * will be transposed towards the second part of the matrix.  Then it is ready for the transform in the Z-direction.
+ * Zero-padding in X- and/or Y-direction is assumed.
+ */
+void yz_transpose_in_place_inv(float *data, 
+                               int *size, 
+                               int *pSSize
+                               );
+#ifdef __cplusplus
+}
+#endif
+#endif
