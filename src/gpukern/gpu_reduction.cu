3af04c1a609f01d74670edf872fb6b4f0ba58568
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-09-16 12:17:38 +0200

    gpu_reduction copyright notice for CUDPP

--- src/gpukern/gpu_reduction.cu
+++ src/gpukern/gpu_reduction.cu
@@ -1,11 +1,105 @@
+// The code in this source file is based on the reduction code from the CUDPP library. Hence the following notice:
+
+/*
+CUDA Data-Parallel Primitives Library (CUDPP) is the proprietary property of The Regents of the University of California ("The Regents") and NVIDIA Corporation ("NVIDIA").
+Copyright (c) 2007 The Regents of the University of California, Davis campus and NVIDIA Corporation. All Rights Reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+- Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+- Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+- Neither the name of The Regents, NVIDIA, nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+The end-user understands that the program was developed for research purposes and is advised not to rely exclusively on the program for any reason.
+
+THE SOFTWARE PROVIDED IS ON AN "AS IS" BASIS, AND THE REGENTS, NVIDIA AND CONTRIBUTORS HAVE NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT, UPDATES, ENHANCEMENTS, OR MODIFICATIONS. THE REGENTS, NVIDIA AND CONTRIBUTORS SPECIFICALLY DISCLAIM ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE REGENTS, NVIDIA OR CONTRIBUTORS BE LIABLE TO ANY PARTY FOR DIRECT, INDIRECT, SPECIAL, INCIDENTAL, EXEMPLARY OR
+CONSEQUENTIAL DAMAGES, INCLUDING BUT NOT LIMITED TO PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES, LOSS OF USE, DATA OR PROFITS, OR BUSINESS INTERRUPTION, HOWEVER CAUSED AND UNDER ANY THEORY OF LIABILITY WHETHER IN CONTRACT, STRICT LIABILITY OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE AND ITS DOCUMENTATION, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+If you do not agree to these terms, do not download or use the software.  This license may be modified only in a writing signed by authorized signatory of all parties.  For The Regents contact copyright@ucdavis.edu.
+
+Relating to funding received by the Regents- Acknowledgment: This material is based upon work supported by the Department of Energy under Award Numbers DE-FG02-04ER25609 and DE-FC02-06ER25777.
+
+Disclaimer: This report was prepared as an account of work sponsored by an agency of the United States Government. Neither the United States Government nor any agency thereof, nor any of their employees,
+makes any warranty, express or implied, or assumes any legal liability or responsibility for the accuracy, completeness, or usefulness of any information, apparatus, product, or process disclosed, or represents that its use would not infringe privately owned rights. Reference
+herein to any specific commercial product, process, or service by trade name, trademark, manufacturer, or otherwise does not necessarily
+constitute or imply its endorsement, recommendation, or favoring by the United States Government or any agency thereof. The views and opinions of authors expressed herein do not necessarily state or reflect those of the United States Government or any agency hereof.
+*/
+
+// This code has been significantly modified from its original version.
+
 #include "gpu_reduction.h"
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+  #endif
+
+
+  gpu_reduce(int size, int threads, int blocks, float* d_idata, float* d_odata) {
+    dim3 dimBlock(threads, 1, 1);
+    dim3 dimGrid(blocks, 1, 1);
+
+    // when there is only one warp per block, we need to allocate two warps
+    // worth of shared memory so that we don't index shared memory out of bounds
+    int smemSize = (threads <= 32) ? 2 * threads * sizeof(T) : threads * sizeof(T);
+
+    if (isPow2(size))
+    {
+      switch (threads)
+      {
+        case 512:
+          reduce6<T, 512, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 256:
+          reduce6<T, 256, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 128:
+          reduce6<T, 128, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 64:
+          reduce6<T,  64, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 32:
+          reduce6<T,  32, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 16:
+          reduce6<T,  16, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  8:
+          reduce6<T,   8, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  4:
+          reduce6<T,   4, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  2:
+          reduce6<T,   2, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  1:
+          reduce6<T,   1, true><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      }
+    }
+    else
+    {
+      switch (threads)
+      {
+        case 512:
+          reduce6<T, 512, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 256:
+          reduce6<T, 256, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 128:
+          reduce6<T, 128, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 64:
+          reduce6<T,  64, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 32:
+          reduce6<T,  32, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case 16:
+          reduce6<T,  16, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  8:
+          reduce6<T,   8, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  4:
+          reduce6<T,   4, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  2:
+          reduce6<T,   2, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+        case  1:
+          reduce6<T,   1, false><<< dimGrid, dimBlock, smemSize >>>(d_idata, d_odata, size); break;
+      }
+    }
+  }
 
 
 
-#ifdef __cplusplus
+  #ifdef __cplusplus
 }
 #endif
