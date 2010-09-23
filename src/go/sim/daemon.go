d8e5fc397bd009a860960f9e797f81409c9af7ab
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-09-23 09:40:56 +0200

    daemon: rudimentary searching for input files

--- src/go/sim/daemon.go
+++ src/go/sim/daemon.go
@@ -1,7 +1,46 @@
 package sim
 
-import ()
+import (
+  "flag"
+  "os"
+  "fmt"
+  "strings"
+)
+
 
 func DaemonMain() {
+  watchdirs := make([]string, flag.NArg())
+  for i:=range watchdirs{
+    watchdirs [i] = flag.Arg(i)
+  }
+  // if no watch dir is specified, look in the current directory
+  if len(watchdirs) == 0{
+    watchdirs = []string{"."}
+  }
+
+  infile := findInputFile(watchdirs[0])
+  fmt.Println(infile)
+}
+
+// Searches for a pending input file in the given directory.
+// Looks for a file ending in ".in" for which no corresponding
+// ".out" file exists yet.
+// Returns an empty string when no suitable input file is present.
+func findInputFile(dir string) string{
+  d, err := os.Open(dir, os.O_RDONLY, 0666)
+  if err != nil {
+    Debugv(err)
+    return ""
+  }
+  defer d.Close()
   
+  // loop over Readdirnames(), all files in the directory
+  for filenames, err2 := d.Readdirnames(1); err2 == nil; filenames, err2 = d.Readdirnames(1){
+    file := filenames[0]
+    if strings.HasSuffix(file, ".in"){
+      return file
+    }
+  }
+
+  return ""
 }
