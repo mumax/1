93ba1bc8c3431bf909433786a6cf60e75dcb6028
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-10-12 12:42:25 +0200

    outtools --recover "string" prints string when an error occurs

--- src/go/outtools/main.go
+++ src/go/outtools/main.go
@@ -11,6 +11,7 @@ import (
 	. "tensor2"
 	"fmt"
 	. "math"
+	"os"
 )
 
 
@@ -21,16 +22,37 @@ func main() {
 
 	commands, args := refsh.ParseFlags()
 	for i := range commands {
-		ret := sh.Call(commands[i], args[i])
-		for _,r := range ret{
-      fmt.Print(r, " ")
-    }
-    fmt.Println()
+    
+		func() {
+
+      defer func() {
+				if main_.recover_val != "" {
+          err := recover()
+					if err != nil { fmt.Fprintln(os.Stderr, err); fmt.Println(main_.recover_val)}
+				}
+			}()
+
+			ret := sh.Call(commands[i], args[i])
+			if ret == nil {
+				ret = []interface{}{main_.recover_val}
+			}
+			for _, r := range ret {
+				fmt.Print(r, " ")
+			}
+			if len(ret) != 0 {fmt.Println()}
+		}()
+		
 	}
 }
 
+func (m *Main) Recover(value string) {
+	m.recover_val = value
+}
 
-func (m *Main) CorePol(fname string) float64 {
+
+// returns the vortex core polarization
+// (value of max out-of-plane magnetization and a string "#up" or "down")
+func (m *Main) CorePol(fname string) (maxMz float64, updown string) {
 	data := ToT4(FReadAscii(fname))
 	array := data.Array
 	mz := array[0]
@@ -44,12 +66,18 @@ func (m *Main) CorePol(fname string) float64 {
 			}
 		}
 	}
-	return answer
+	maxMz = answer
+	if maxMz > 0. {
+		updown = "#up"
+	} else {
+		updown = "#down"
+	}
+	return
 }
 
 
 type Main struct {
-	
+	recover_val string
 }
 
 func NewMain() *Main {
