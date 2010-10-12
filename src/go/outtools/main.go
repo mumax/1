717911656dcc0ad0c8492b541672e49eafcdf100
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-10-12 10:26:15 +0200

    introducing outtools

---
+++ src/go/outtools/main.go
@@ -0,0 +1,33 @@
+//  Copyright 2010  Arne Vansteenkiste
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package main
+
+import (
+  "refsh"
+)
+
+
+func main() {
+  refsh := refsh.New()
+  main_ := NewMain()
+  refsh.AddAllMethods(main_)
+  refsh.ExecFlags()
+}
+
+func (m *Main) ReadBinary(fname string){
+  
+}
+
+
+
+type Main struct{
+  value interface{}
+}
+
+func NewMain() *Main{
+  return new(Main)
+}
