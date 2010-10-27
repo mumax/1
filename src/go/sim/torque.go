a164fe4a1d0baef9ef3605dd8e2eba7a4bab4a95
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-10-27 11:52:41 +0200

    Torque uses DeltaM instead of deltaM so it will uses spintorque automatically when needed

--- src/go/sim/torque.go
+++ src/go/sim/torque.go
@@ -21,6 +21,5 @@ func (dev *Sim) DeltaM(m, h *DevTensor, alpha, dtGilbert float32) {
 func (dev *Sim) Torque(m, h *DevTensor, alpha float32) {
 	assert(len(m.size) == 4)
 	assert(tensor.EqualSize(m.size, h.size))
-	N := m.size[1] * m.size[2] * m.size[3]
-	dev.deltaM(m.data, h.data, alpha, 1.0, N) // we (ab)use DeltaM with dt=1.
+	dev.DeltaM(m, h, alpha, 1.0) // we (ab)use DeltaM with dt=1.
 }
