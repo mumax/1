5c2c10508319ad42e69b1a7c76f91a486101fd8b
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-10-28 14:43:57 +0200

    automatically select torque() / spintorque(), depending on whether the current density is zero

--- src/go/sim/torque.go
+++ src/go/sim/torque.go
@@ -10,32 +10,38 @@ import (
 // and DeltaM
 
 
-// calculates torque * dt, overwrites h with the result
+// Calculates torque * dt, overwrites h with the result
+// Automatically uses the Spintorque version when necessary
 func (s *Sim) DeltaM(m, h *DevTensor, dt float32) {
-  //TODO smart switch between torque/spintorque
 
-  s.SpintorqueDeltaM(m, h, dt)
-  
-// 	assert(len(m.size) == 4)
-// 	assert(tensor.EqualSize(m.size, h.size))
-// 	N := m.size[1] * m.size[2] * m.size[3]
-// 	alpha := s.alpha
-// 	dtGilbert := dt / (1 + alpha*alpha)
-// 	s.deltaM(m.data, h.data, alpha, dtGilbert, N)
+  assert(len(m.size) == 4)
+  assert(tensor.EqualSize(m.size, h.size))
+
+  // If the current density is nonzero, we use the spin-transfer torque,
+  // otherwise use the Landau-Lifschitz torque.
+  // Of course, the spin-transfer torque term with zero current density
+  // gives the same result as the LL torque, but is slightly slower. 
+  if s.input.j[0] != 0 || s.input.j[1] != 0 || s.input.j[2] != 0 {
+    s.SpintorqueDeltaM(m, h, dt)
+  }else{
+    N := m.size[1] * m.size[2] * m.size[3]
+    alpha := s.alpha
+    dtGilbert := dt / (1 + alpha*alpha)
+    s.deltaM(m.data, h.data, alpha, dtGilbert, N)
+  }
 }
 
 
 // overwrites h with torque(m, h) * dtGilbert, inculding spin-transfer torque terms.
+// here be dragons:
 // dtGilb = dt / (1+alpha^2)
 // alpha = damping
 // beta = b(1+alpha*xi)
 // epsillon = b(xi-alpha)
 // b = µB / e * Ms (Bohr magneton, electron charge, saturation magnetization)
 // u = current density / (2*cell size)
-// here be dragons
+// TODO we could cache some parameters
 func (s *Sim) SpintorqueDeltaM(m, h *DevTensor, dt float32) {
-	assert(len(m.size) == 4)
-	assert(tensor.EqualSize(m.size, h.size))
 
 	alpha := s.alpha
 	dtGilb := dt / (1 + alpha*alpha)
