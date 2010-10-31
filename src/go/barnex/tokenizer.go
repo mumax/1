80dd578822c4dd2baa9ef1fa8ab5ac095642fcb8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-10-31 09:16:36 +0100

    working on tokenizer

--- src/go/barnex/tokenizer.go
+++ src/go/barnex/tokenizer.go
@@ -1,33 +1,47 @@
 package barnex
 
+import (
+	"fmt"
+)
 
+func Tokenize(str string) {
 
-func Tokenize(str string) (root *Node) {
-//   t := newTokenizer(str)
-  return
+	for t := newTokenizer(str); t.currchr != -1; t.nextChr() {
+		fmt.Print(t.currchr)
+	}
+	return
 }
 
-func newTokenizer(str string) *tokenizer{
-  t := new(tokenizer)
-  t.str = str
+func newTokenizer(str string) *tokenizer {
+	t := new(tokenizer)
+	t.str = str
 
-  t.pos = 0
-  t.prev = -1 // no previous character
-  t.curr = -1
-  t.next = -1
-  
-  if len(str) > 0{
-      t.curr = int(str[t.pos])
-  }
-  if len(str) > 1{
-    t.next = int(str[t.pos+1])
-  }
-  
-  return t
+	t.pos = 0
+	t.prevchr = -1 // no prevchrious character
+	t.currchr = -1
+	t.nextchr = -1
+
+	if len(str) > 0 {
+		t.currchr = int(str[t.pos])
+	}
+	if len(str) > 1 {
+		t.nextchr = int(str[t.pos+1])
+	}
+
+	return t
+}
+
+func (t *tokenizer) nextChr() {
+	t.prevchr = t.currchr
+	t.currchr = t.nextchr
+	if len(t.str) > t.pos {
+		t.pos++
+		t.nextchr = int(t.str[t.pos])
+	}
 }
 
 type tokenizer struct {
-  str string
-  pos int
-  prev, curr, next int
+	str                       string
+	pos                       int
+	prevchr, currchr, nextchr int
 }
