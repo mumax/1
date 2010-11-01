38b5ecd7e2f44d71b1946f28603dee7655df8311
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-11-01 11:52:52 +0100

    minor

--- src/go/barnex/tokenize_test.go
+++ src/go/barnex/tokenize_test.go
@@ -2,10 +2,23 @@ package barnex
 
 import (
 	"testing"
+	"go/scanner"
+	"go/token"
+	"fmt"
 )
 
 
 func TestTokenize(t *testing.T) {
-	t := newTokenizer(str)
-	Tokenize("Hello World")
+	var s scanner.Scanner
+	handler := ErrHandler{}
+	s.Init(fname, souce, handler, 0)
+	
+}
+
+type ErrHandler struct{
+
+}
+
+func (e *ErrHandler) Error(pos token.Position, msg string){
+  fmt.Println(pos, msg)
 }
