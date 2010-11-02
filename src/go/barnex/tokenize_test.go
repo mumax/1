c6e1ed6a0e9acde324338a93a1bbfd31ad2de8a5
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-11-02 17:05:19 +0100

    tokenizer

--- src/go/barnex/tokenize_test.go
+++ src/go/barnex/tokenize_test.go
@@ -2,34 +2,14 @@ package barnex
 
 import (
 	"testing"
-	"go/scanner"
-	"go/token"
-	"io/ioutil"
 	"fmt"
 )
 
 
 func TestTokenize(t *testing.T) {
-  fname := "test.txt"
-  source, err := ioutil.ReadFile(fname)
-  if err != nil{
-    panic(err)
+  node := Tokenize("test.txt")
+  for node != nil{
+    fmt.Println(node)
+    node = node.Next
   }
-	var s scanner.Scanner
-	handler := ErrHandler{}
-	s.Init(fname, source, handler, scanner.InsertSemis)
-
-	pos, tok, lit := s.Scan()
-  for tok != token.EOF{
-    fmt.Println(pos, tok, string(lit))
-    pos, tok, lit = s.Scan()
-  }
-}
-
-type ErrHandler struct{
-
-}
-
-func (e ErrHandler) Error(pos token.Position, msg string){
-  fmt.Println(pos, msg)
 }
--- src/go/barnex/tokenizer.go
+++ src/go/barnex/tokenizer.go
@@ -1,57 +1,45 @@
 package barnex
 
-// import (
-// 	"fmt"
-// )
-// 
-// func Tokenize(str string) {
-// 	t := newTokenizer(str)
-// 
-// }
-// 
-// func newTokenizer(str string) *tokenizer {
-// 	t := new(tokenizer)
-// 	t.str = str
-// 
-// 	t.pos = 0
-// 	t.prevchr = -1 // no prevchrious character
-// 	t.currchr = -1
-// 	t.nextchr = -1
-// 
-// 	if len(str) > 0 {
-// 		t.currchr = int(str[t.pos])
-// 	}
-// 	if len(str) > 1 {
-// 		t.nextchr = int(str[t.pos+1])
-// 	}
-// 
-// 	return t
-// }
-// 
-// func (t *tokenizer) skipWhitespace() {
-// 	for IsWhitespace(t.currchr) {
-// 		t.nextChr()
-// 	}
-// }
-// 
-// // Advances the tokenizer to the next character,
-// // which can be retrieved with tokenizer.currchr
-// func (t *tokenizer) nextChr() {
-// 	t.prevchr = t.currchr
-// 	t.currchr = t.nextchr
-// 	if len(t.str) > t.pos+2 {
-// 		t.pos++
-// 		t.nextchr = int(t.str[t.pos+1])
-// 	} else {
-// 		t.nextchr = -1
-// 	}
-// }
-// 
-// type tokenizer struct {
-// 	str string // string being tokenized
-// 
-// 	pos              int // position of currchr in string
-// 	currchr          int // current character
-// 	prevchr, nextchr int // previous/next character
-// 
-// }
+import (
+  "go/scanner"
+  "go/token"
+  "io/ioutil"
+  "fmt"
+)
+
+
+func Tokenize(fname string) *Node{
+  source, err := ioutil.ReadFile(fname)
+  if err != nil{
+    panic(err)
+  }
+  var s scanner.Scanner
+  handler := ErrHandler{}
+  s.Init(fname, source, handler, scanner.InsertSemis)
+
+  
+  root := NewRootNode()
+  prev := root
+
+  pos, tok, lit := s.Scan()
+  for tok != token.EOF{
+    fmt.Println(pos, tok, lit)
+    node := NewNode(pos, tok, string(lit))
+    node.Prev = prev
+    node.Prev.Next = node
+    pos, tok, lit = s.Scan()
+    prev = node
+  }
+
+  return root
+}
+
+
+type ErrHandler struct{
+
+}
+
+func (e ErrHandler) Error(pos token.Position, msg string){
+  fmt.Println(pos, msg)
+  panic(msg)
+}
