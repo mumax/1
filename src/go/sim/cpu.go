1752ba1c43737542a539130bddb47bffa3488247
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-11-18 10:48:18 +0100

    merged devarne, need FFT delete functions

--- src/go/sim/cpu.go
+++ src/go/sim/cpu.go
@@ -92,7 +92,8 @@ func (d Cpu) semianalStep(m, h uintptr, dt, alpha float32, order, N int) {
 	default:
 		panic(fmt.Sprintf("Unknown semianal order:", order))
 	case 0:
-		C.cpu_anal_fw_step_unsafe((*C.float)(unsafe.Pointer(m)), (*C.float)(unsafe.Pointer(h)), C.float(dt), C.float(alpha), C.int(N))
+    panic("unimplemented")
+		//C.cpu_anal_fw_step_unsafe((*C.float)(unsafe.Pointer(m)), (*C.float)(unsafe.Pointer(h)), C.float(dt), C.float(alpha), C.int(N))
 	}
 }
 
