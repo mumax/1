d79f0ae67b908b9f04b1da7b7159fc7b6ad6fa33
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-11-30 10:38:13 +0100

    bugreport: multi-threaded cpu code is not faster than single-threaded

--- src/cpukern/cpu_init.cpp
+++ src/cpukern/cpu_init.cpp
@@ -1,6 +1,7 @@
 #include "cpu_init.h"
 #include <sys/sysinfo.h>
 #include "fftw3.h"
+#include <omp.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -8,8 +9,9 @@ extern "C" {
 
 void cpu_init(){
   // set up multi-threaded fftw
-  fftwf_init_threads();
-  fftwf_plan_with_nthreads(get_nprocs_conf()); // automatically determines the available number of CPU's
+    fftwf_init_threads();
+    fftwf_plan_with_nthreads(get_nprocs_conf()); // automatically determines the available number of CPU's
+//  omp_set_num_threads(1);
 }
 
 #ifdef __cplusplus
