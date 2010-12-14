50de5bafe72302f663b8d875a4027a6b235b9823
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-12-14 20:36:23 +0100

    fixed runtime dynamic link error

--- dep/go/src/pkg/runtime/freebsd/os.h
+++
@@ -1,5 +0,0 @@
-int32 runtime·thr_new(ThrParam*, int32);
-void runtime·sigpanic(void);
-void runtime·sigaltstack(Sigaltstack*, Sigaltstack*);
-struct sigaction;
-void runtime·sigaction(int32, struct sigaction*, struct sigaction*);
