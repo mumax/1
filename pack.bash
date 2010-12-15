d7598dc446675e0b7f35e978f07feda850c95a21
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-12-15 11:46:09 +0100

    added stdprobl4 to examples

--- pack.bash
+++ pack.bash
@@ -13,15 +13,7 @@ mkdir $OUT
 cp setup_64bit.bash $OUT
 cp -r bin $OUT
 cp -r lib $OUT
-#cp -r dep/cuda $OUT/dep
-#cp -r dep/go $OUT/dep
 
-rm -rf $OUT/dep/go/src
-rm -rf $OUT/dep/go/test
-rm -rf $OUT/bin/go
-rm -rf $OUT/dep/fftw
-rm -rf $OUT/dep/cuda/devdriver*
-rm -rf $OUT/dep/cuda/include
 
 mkdir $OUT/examples
 cp -r examples/*.in $OUT/examples
