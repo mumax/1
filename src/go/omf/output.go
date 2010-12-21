99e969082cf274b0083b43e3c26d3348f85eed17
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2010-12-21 14:14:04 +0100

    .omf data order bugfix

--- src/go/omf/output.go
+++ src/go/omf/output.go
@@ -74,11 +74,13 @@ func (c *OmfCodec) Encode(out_ io.Writer, f Interface) {
 
 	hdr(out, "Begin", "Data "+format)
 
-  for i:=0; i<gridsize[Z]; i++{
+  // Here we loop over X,Y,Z, not Z,Y,X, because
+  // internal in C-order == external in Fortran-order
+  for i:=0; i<gridsize[X]; i++{
     for j:=0; j<gridsize[Y]; j++{
-      for k:=0; k<gridsize[X]; k++{
-        for c:=0; c<3; c++{
-          fmt.Fprint(out, data[c][k][j][i], " ")
+      for k:=0; k<gridsize[Z]; k++{
+        for c:=Z; c>=X; c--{
+          fmt.Fprint(out, data[c][i][j][k], " ")
         }
         fmt.Fprint(out, "\t")
       }
