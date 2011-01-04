da36978ebd269f2784fdbe86ba6664cef9cd5dcd
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-01-04 17:04:08 +0100

    template works with one value

--- src/go/template/main.go
+++ src/go/template/main.go
@@ -7,42 +7,89 @@
 package main
 
 import (
-  "fmt"
-  "flag"
-  "io/ioutil"
-  "os"
+	"fmt"
+	"flag"
+	"io/ioutil"
+	"iotool"
+	"os"
+	"strings"
 )
 
 
 func main() {
-  if flag.NArg() == 0{
-    fmt.Fprintln(os.Stderr, "No input files.")
-    fmt.Fprintln(os.Stderr, USAGE)
-    os.Exit(-1)
-  }
-  
-  file := flag.Arg(flag.NArg()-1)
-  bytes, err := ioutil.ReadFile(file)
-  content := string(bytes)
-  
-  if err != nil{
-    panic(err)
-  }
-  
-//   for i:=0; i<flag.NArg()-1; i++{
-// 
-//   }
+	if flag.NArg() == 0 {
+		Error("No input files.")
+	}
 
-  fmt.Println(content)
+	file := flag.Arg(flag.NArg() - 1)
+	bytes, err := ioutil.ReadFile(file)
+	content := string(bytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	docs := []*Document{&Document{content, file}}
+
+	for f := 0; f < flag.NArg()-1; f++ {
+		flag := flag.Arg(f)
+		split := strings.Split(flag, "=", 2)
+		if len(split) != 2 {
+			Error("Syntax error: expecting \"key=value\"")
+		}
+		key, val := split[0], split[1]
+
+		len_docs := len(docs) // no need to iterate over those that will be added by this loop
+		for d := 0; d < len_docs; d++ {
+			doc := docs[d]
+			if doc != nil {
+				docs[d] = nil
+				docs = append(docs, doc.Replace(key, val))
+			}
+		}
+	}
+
+	for _, d := range docs {
+		if d != nil {
+      if strings.Contains(d.content, "{"){
+        Error("Not all {key}'s were specified.")
+        // TODO: it might be nice to show which ones...
+      }
+			out := iotool.MustOpenWRONLY(d.name + ".in")
+			defer out.Close()
+			out.Write([]byte(d.content))
+		}
+	}
+
+}
+
+
+type Document struct {
+	content string
+	name    string
+}
+
+func (d *Document) Replace(key, val string) *Document {
+	if !strings.Contains(d.content, key) {
+		Error("Template file does not contain key: {" + key + "}")
+	}
+	d2 := new(Document)
+	d2.content = strings.Replace(d.content, "{"+key+"}", val, -1)
+	d2.name = d.name + "_" + key + "=" + val
+	return d2
+}
+
+func Error(msg ...interface{}) {
+	fmt.Fprintln(os.Stderr, msg...)
+	fmt.Fprintln(os.Stderr, USAGE)
+	os.Exit(-1)
 }
 
 const USAGE = `
 Usage: template file should contain {key} statements, where "key" can be replaced by any identifier.
-
 template key=value1,value2,... file.in  Creates files where {key} is replaced by each of the values.
 template key=start:stop file.in         Replaces key by all integers between start and stop (exclusive).
 template key=start:stop:step file.in    As above but with a step different from 1.
 template key1=... key2=...              Multiple keys may be specified.
-
 Output files are given automaticially generated names.
 `
