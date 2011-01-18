03ccd57ce5cce653446c991b67cda17d107f3fe6
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-01-18 16:37:41 +0100

    sucessfull communication to python stdin

--- src/go/sim/main_python.go
+++ src/go/sim/main_python.go
@@ -8,7 +8,7 @@ package sim
 
 import(
 	"exec"
-	"iotool"
+//	"iotool"
 	"fmt"
 	"os"
 )
@@ -19,25 +19,19 @@ func main_python(infile string) {
 	outdir := RemoveExtension(infile) + ".out"
 	os.Mkdir(outdir, 0777)//TODO: permission?
 
-	fname_to_py := outdir + "/" + ".fifo-to-python.py"
-	syscommand("mkfifo", []string{fname_to_py})
-
-	py_args := []string{fname_to_py}
+	py_args := []string{}
 	py_bin, errlook := exec.LookPath("python")
 	Check(errlook, ERR_SUBPROCESS)
 	fmt.Println("starting ", py_bin)
-	python, errpy := subprocess(py_bin, py_args, exec.DevNull, exec.Pipe, exec.Pipe)
+	python, errpy := subprocess(py_bin, py_args, exec.Pipe, exec.Pipe, exec.Pipe)
 	Check(errpy, ERR_SUBPROCESS)
 	fmt.Println("python PID: ", python.Pid)
 	go Pipe(python.Stdout, os.Stdout) // TODO: logging etc
 	go Pipe(python.Stderr, os.Stderr)
+	
 
-	fmt.Println("Opening FIFO: ", fname_to_py)
-    fifo_to_py := iotool.MustOpenWRONLY(fname_to_py)
-	fmt.Println("Opened FIFO: ", fname_to_py)
-
-	fmt.Fprintln(fifo_to_py, "kakaprint(1+1)\n")
-	fifo_to_py.Close()
+	fmt.Fprintln(python.Stdin, "print(1+1)\n")
+	python.Stdin.Close()
 
 	_, errwait := python.Wait(0) // Wait for exit
 	Check(errwait, ERR_SUBPROCESS)
