6c46e873ca8715d0b06f95aeede71eb027b53172
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-01-18 16:35:18 +0100

    experimenting with FIFO to python

--- src/go/sim/main_python.go
+++ src/go/sim/main_python.go
@@ -17,20 +17,27 @@ import(
 func main_python(infile string) {
 
 	outdir := RemoveExtension(infile) + ".out"
+	os.Mkdir(outdir, 0777)//TODO: permission?
+
 	fname_to_py := outdir + "/" + ".fifo-to-python.py"
 	syscommand("mkfifo", []string{fname_to_py})
-    fifo_to_py := iotool.MustOpenWRONLY(fname_to_py)
-
 
 	py_args := []string{fname_to_py}
 	py_bin, errlook := exec.LookPath("python")
 	Check(errlook, ERR_SUBPROCESS)
+	fmt.Println("starting ", py_bin)
 	python, errpy := subprocess(py_bin, py_args, exec.DevNull, exec.Pipe, exec.Pipe)
 	Check(errpy, ERR_SUBPROCESS)
+	fmt.Println("python PID: ", python.Pid)
 	go Pipe(python.Stdout, os.Stdout) // TODO: logging etc
 	go Pipe(python.Stderr, os.Stderr)
 
-	fmt.Fprintln(fifo_to_py, "print(1+1)\n")
+	fmt.Println("Opening FIFO: ", fname_to_py)
+    fifo_to_py := iotool.MustOpenWRONLY(fname_to_py)
+	fmt.Println("Opened FIFO: ", fname_to_py)
+
+	fmt.Fprintln(fifo_to_py, "kakaprint(1+1)\n")
+	fifo_to_py.Close()
 
 	_, errwait := python.Wait(0) // Wait for exit
 	Check(errwait, ERR_SUBPROCESS)
