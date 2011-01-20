236370ac69d4e88e1c0e0f9ace99db795a4ce2bb
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-01-20 11:47:00 +0100

    device

--- src/go/mumax/device/device.go
+++ src/go/mumax/device/device.go
@@ -1,18 +1,23 @@
+//  This file is part of MuMax, a high-perfomrance micromagnetic simulator.
 //  Copyright 2010  Arne Vansteenkiste
 //  Use of this source code is governed by the GNU General Public License version 3
 //  (as published by the Free Software Foundation) that can be found in the license.txt file.
 //  Note that you are welcome to modify this code under the condition that you do not remove any 
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
-package sim
+package device
 
 import (
-	"tensor"
-	"unsafe"
-	//"fmt"
 )
 
-/**
+// The device
+var device Interface
+
+func UseGpu(gpu_id int){
+	
+}
+
+/*
  * A Backend wraps some unsafe methods of the Device interface
  * with safe versions, and provides additional methods derived
  * from low-end Devices methods. Therefore, the user should
