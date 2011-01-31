1699bb67b667060f3ccf40abe5556376c0de530c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-01-31 15:56:06 +0100

    anisotropy selftest

--- src/go/sim/main_master.go
+++ src/go/sim/main_master.go
@@ -25,7 +25,7 @@ import (
 )
 
 const WELCOME = `
-  MuMax 0.4.1839
+  MuMax 0.4.1874
   (c) Arne Vansteenkiste & Ben Van de Wiele,
       DyNaMat/EELAB UGent
   This version is meant for internal testing purposes only,
