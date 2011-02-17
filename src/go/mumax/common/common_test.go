7861c2b21f66c79273d37c94bf1a7a5b4a3ce94e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-02-17 15:33:50 +0100

    kernel_init Makefile

---
+++ src/go/mumax/common/common_test.go
@@ -0,0 +1,21 @@
+//  This file is part of MuMax, a high-performance micromagnetic simulator.
+//  Copyright 2010  Arne Vansteenkiste
+//  Use of this source code is governed by the GNU General Public License version 3
+//  (as published by the Free Software Foundation) that can be found in the license.txt file.
+//  Note that you are welcome to modify this code under the condition that you do not remove any 
+//  copyright notices and prominently state that you modified it, giving a relevant date.
+
+package common
+
+import(
+	"testing"
+)
+
+func TestIsFinite(t *testing.T){
+	var one float32 = 1
+	var zero float32 = 0
+
+	if !IsReal(0.) {t.Fail()}
+	if !IsReal(1.) {t.Fail()}
+	if IsReal(zero/ {t.Fail()}
+}
