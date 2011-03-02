8fdfbb1d31a19e0746801c6b1229f3c8b86fa3fc
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-03-02 11:40:52 +0100

    odttool meshdom works

--- src/go/odttools/filter.go
+++ src/go/odttools/filter.go
@@ -69,17 +69,20 @@ func (s Set) ToArray() []float32 {
 }
 
 
+// Like Matrix() but outputs octave/matlab commands that set
+// x, y and data for use in surf(x,y,data)
 func Meshdom(i_colname, j_colname, data_colname string) {
 	matrix(i_colname, j_colname, data_colname, true)
 }
 
-func Matrix(i_colname, j_colname, data_colname string, octave_format bool) {
-	matrix(i_colname, j_colname, data_colname, false)
-}
 
 // Assuming columns i,j contain matrix indices,
 // coutput column data in a correspondig 2D grid.
 // Missing values become 0.
+func Matrix(i_colname, j_colname, data_colname string, octave_format bool) {
+	matrix(i_colname, j_colname, data_colname, false)
+}
+
 func matrix(i_colname, j_colname, data_colname string, octave_format bool) {
 	i_col := table.GetColumnIndex(i_colname)
 	j_col := table.GetColumnIndex(j_colname)
@@ -101,6 +104,26 @@ func matrix(i_colname, j_colname, data_colname string, octave_format bool) {
 	}
 	J = setJ.ToArray()
 
+	if octave_format{
+		fmt.Print("x=[")	
+		for i := range I{
+			if i != 0{
+				fmt.Print(", ")
+			}
+			fmt.Print(I[i])
+		}
+		fmt.Println("];")
+
+		fmt.Print("y=[")	
+		for i := range J{
+			if i != 0{
+				fmt.Print(", ")
+			}
+			fmt.Print(J[i])
+		}
+		fmt.Println("];")
+	}
+	
 	// (2) Make the "outer product" of the two index sets,
 	// spanning a matrix that can be index with each possible i,j pair
 	// (even those not present in the input, their data will be 0.)
@@ -118,7 +141,6 @@ func matrix(i_colname, j_colname, data_colname string, octave_format bool) {
 	// to the corresponding i,j position of the matrix. (j, data on the same line as i)
 	// Missing pairs keep 0. as data.
 	D := table.Column[data_col]
-		
 	for i := range table.Column[i_col] {
 		matrix[table.Column[i_col][i]][table.Column[j_col][i]] = D[i]
 	}
