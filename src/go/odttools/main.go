1ecafe5896965e49e6089173794da897c8e7f92f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-03-15 10:32:10 +0100

    remove stray py files

--- src/go/odttools/main.go
+++ src/go/odttools/main.go
@@ -61,6 +61,7 @@ func main() {
 	sh.AddFunc("diff2", Diff2)
 	sh.AddFunc("avgdiff2", AvgDiff2)
 	sh.AddFunc("avgdiff2nopeak", AvgDiff2NoPeak)
+	sh.AddFunc("inplanerms", InplaneRMS)
 	cmd, args, files := refsh.ParseFlags2()
 
 	// Each file is read and stored in "data".
