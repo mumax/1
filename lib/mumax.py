019726770f5581c62aaba07cfe5a129d2a3d9a8a
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-03-15 15:58:15 +0100

    mumax.py

--- lib/mumax.py
+++ lib/mumax.py
@@ -222,6 +222,10 @@ def applystatic(what, bx, by, bz):
 def applyrf(what, bx, by, bz, freq):
 	send("applyrf", [what, bx, by, bz, freq])
 
+# Apply an RF field/current, slowly ramped in
+def applyrframp(what, bx, by, bz, freq, ramptime):
+	send("applyrframp", [what, bx, by, bz, freq, ramptime])
+
 # Apply a rotating field/current
 def applyrotating(what, bx, by, bz, freq, phaseX, phaseY, phaseZ):
 	send("applyrotating", [what, bx, by, bz, freq, phaseX, phaseY, phaseZ])
