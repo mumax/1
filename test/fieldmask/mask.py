144c3a892468bc90476e3d3f03e02c61c3a6e44c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2011-04-06 11:33:29 +0200

    space dependent field also on CPU

--- test/fieldmask/mask.py
+++ test/fieldmask/mask.py
@@ -1,10 +1,13 @@
 from mumax import *
 
+tabulate('E', True)
+
 # material
 msat(800e3)   
 aexch(1.3e-11)     
 alpha(0.02)
-
+k1(50e3)
+anisuniaxial(0, 1, 0)
 
 # geometry 
 nx = 128
@@ -16,7 +19,7 @@ partsize(500e-9, 500e-9, 3e-9)
 uniform(1, 1, 0)
 alpha(2)
 run(1e-9) # relax
-alpha(0.02)
+alpha(0.001)
 save("m", "text")
 
 
@@ -25,7 +28,7 @@ autosave("m", "omf", 10e-12)
 autosave('table', 'ascii', 5e-12)
 fieldmask("mask.omf")
 applystatic('field', -10, 0, 0)
-run(1e-9)
+run(10e-9)
 
 
 
