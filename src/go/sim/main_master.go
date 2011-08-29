c8d0dda0fa9089ca29ef099c8e81c1e331c6ddd8
Arne <Arne.Vansteenkiste@UGent.be>
2011-08-29 13:55:06 +0200

    version number

--- src/go/sim/main_master.go
+++ src/go/sim/main_master.go
@@ -26,10 +26,10 @@ import (
 	"fmt"
 )
 
-const VERSION = 2468
+const VERSION = 2470
 
 const WELCOME = `
-  MuMax 0.9.2468
+  MuMax 0.9.2470
   (c) Arne Vansteenkiste & Ben Van de Wiele,
       DyNaMat/EELAB Ghent University.
 
