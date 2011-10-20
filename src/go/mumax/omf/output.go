30a46ddbb7e14a0710eac927b7bdfc2280c8dc9f
Arne Vansteenkiste <arne@dynamag.UGent.be>
2011-10-20 14:53:28 +0200

    update version 1.1

--- src/go/mumax/omf/output.go
+++ src/go/mumax/omf/output.go
@@ -4,6 +4,8 @@
 //  Note that you are welcome to modify this code under the condition that you do not remove any
 //  copyright notices and prominently state that you modified it, giving a relevant date.
 
+// OVF2 suport added by Mykola Dvornik, 2011
+
 package omf
 
 import (
